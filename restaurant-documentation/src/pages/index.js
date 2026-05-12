8a1f1cff5eae2d5003d1da9e8765dea0b159af59
bvictoriao <55405924+bvictoriao@users.noreply.github.com>
2026-05-12 13:15:55 +0300

    Update index.js

--- restaurant-documentation/src/pages/index.js
+++ restaurant-documentation/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/api/rest-api">
+            to="/docs/concept/business-requirements">
             Открыть документацию
           </Link>
         </div>
